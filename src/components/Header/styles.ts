f7d52a754feb71257fd0a39d62f12b48ecd79d4e
unknown <fabioflauzino@hotmail.com>
2022-10-06 00:18:24 -0300

    Header e Layout

---
+++ src/components/Header/styles.ts
@@ -0,0 +1,36 @@
+import styled from "styled-components";
+
+export const HeaderContainer = styled.header`
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+
+    nav{
+        display: flex;
+        gap: 0.5rem;
+
+        a{
+            height: 3rem;
+            width: 3rem;
+
+            display: flex;
+            align-items: center;
+            justify-content: center;
+
+            color: ${props => props.theme['gray-100']};
+
+            border-top: 3px solid transparent;
+            border-bottom: 3px solid transparent;
+            transition: all .1s;
+
+            &:hover{
+                border-bottom: 3px solid ${props => props.theme['green-500']};
+                transition: all .1s;
+            }
+
+            &.active{
+                color: ${props => props.theme['green-500']};
+            }
+        }
+    }
+`
