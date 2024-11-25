7aee0db96b1c9d8b0bcea9a671fd1ffc4d8c4320
Fabio Flauzino <fabio@mfrural.com.br>
2024-11-25 11:24:18 -0300

    contexto entre rotas

--- src/pages/History/index.tsx
+++ src/pages/History/index.tsx
@@ -1,9 +1,15 @@
-import { HistoryContainer, HistoryList, Status } from "./styles";
+import { useContext } from 'react'
+import { CyclesContext } from '../../contexts/CyclesContext'
+import { HistoryContainer, HistoryList, Status } from './styles'
 
 export function History() {
+  const { cycles } = useContext(CyclesContext)
+
   return (
     <HistoryContainer>
       <h1>Meu histórico</h1>
+
+      <pre>{JSON.stringify(cycles, null, 2)}</pre>
 
       <HistoryList>
         <table>
@@ -11,7 +17,7 @@ export function History() {
             <tr>
               <th>Tarefa</th>
               <th>Duração</th>
-              <th>Início</th>
+              <th>Duração</th>
               <th>Status</th>
             </tr>
           </thead>
@@ -19,55 +25,57 @@ export function History() {
             <tr>
               <td>Tarefa</td>
               <td>20 minutos</td>
-              <td>Há cerca de 2 meses</td>
+              <td>Há 2 meses</td>
               <td>
-                <Status statusColor="green">
-                  Concluído
-                </Status>
+                <Status statusColor="green">Concluído</Status>
               </td>
             </tr>
-
             <tr>
               <td>Tarefa</td>
               <td>20 minutos</td>
-              <td>Há cerca de 2 meses</td>
+              <td>Há 2 meses</td>
               <td>
-                <Status statusColor="red">
-                  Interrompido
-                </Status>
+                <Status statusColor="green">Concluído</Status>
               </td>
             </tr>
-
             <tr>
               <td>Tarefa</td>
               <td>20 minutos</td>
-              <td>Há cerca de 2 meses</td>
+              <td>Há 2 meses</td>
               <td>
-                <Status statusColor="yellow">
-                  Em andamento
-                </Status>
+                <Status statusColor="green">Concluído</Status>
               </td>
             </tr>
-
             <tr>
               <td>Tarefa</td>
               <td>20 minutos</td>
-              <td>Há cerca de 2 meses</td>
+              <td>Há 2 meses</td>
               <td>
-                <Status statusColor="green">
-                  Concluído
-                </Status>
+                <Status statusColor="green">Concluído</Status>
               </td>
             </tr>
-
             <tr>
               <td>Tarefa</td>
               <td>20 minutos</td>
-              <td>Há cerca de 2 meses</td>
+              <td>Há 2 meses</td>
               <td>
-                <Status statusColor="green">
-                  Concluído
-                </Status>
+                <Status statusColor="green">Concluído</Status>
+              </td>
+            </tr>
+            <tr>
+              <td>Tarefa</td>
+              <td>20 minutos</td>
+              <td>Há 2 meses</td>
+              <td>
+                <Status statusColor="yellow">Em andamento</Status>
+              </td>
+            </tr>
+            <tr>
+              <td>Tarefa</td>
+              <td>20 minutos</td>
+              <td>Há 2 meses</td>
+              <td>
+                <Status statusColor="red">Interrompido</Status>
               </td>
             </tr>
           </tbody>
