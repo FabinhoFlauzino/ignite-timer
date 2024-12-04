74be06f832a54963e2f9dbac364f101f80bb1d40
Fabio Flauzino <fabio@mfrural.com.br>
2024-12-04 14:49:36 -0300

    Finalizando Projeto

--- src/pages/History/index.tsx
+++ src/pages/History/index.tsx
@@ -1,16 +1,13 @@
 import { useContext } from 'react'
-import { CyclesContext } from '../../contexts/CyclesContext'
-import { HistoryContainer, HistoryList, Status } from './styles'
 import { formatDistanceToNow } from 'date-fns'
 import ptBR from 'date-fns/locale/pt-BR'
-
+import { CyclesContext } from '../../contexts/CyclesContext'
+import { HistoryContainer, HistoryList, Status } from './styles'
 export function History() {
   const { cycles } = useContext(CyclesContext)
-
   return (
     <HistoryContainer>
       <h1>Meu histórico</h1>
-
       <HistoryList>
         <table>
           <thead>
@@ -22,32 +19,31 @@ export function History() {
             </tr>
           </thead>
           <tbody>
-            {
-              cycles.map((cycle) => (
+            {cycles.map((cycle) => {
+              return (
                 <tr key={cycle.id}>
                   <td>{cycle.task}</td>
                   <td>{cycle.minutesAmount} minutos</td>
-                  <td>{formatDistanceToNow(cycle.startDate, {
-                    addSuffix: true,
-                    locale: ptBR,
-                  })}
+                  <td>
+                    {formatDistanceToNow(new Date(cycle.startDate), {
+                      addSuffix: true,
+                      locale: ptBR,
+                    })}
                   </td>
                   <td>
                     {cycle.finishedDate && (
                       <Status statusColor="green">Concluído</Status>
                     )}
-
                     {cycle.interruptedDate && (
                       <Status statusColor="red">Interrompido</Status>
                     )}
-
-                    {(!cycle.finishedDate && !cycle.interruptedDate) && (
+                    {!cycle.finishedDate && !cycle.interruptedDate && (
                       <Status statusColor="yellow">Em andamento</Status>
                     )}
                   </td>
                 </tr>
-              ))
-            }
+              )
+            })}
           </tbody>
         </table>
       </HistoryList>
